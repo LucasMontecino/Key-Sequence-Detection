290097556136374d78281506f7b6904a3a6d0fc3
Lucas Montecino <lucasmontecino95@gmail.com>
2023-09-15 17:34:42 -0300

    funny-keykode-commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const pressed = [];
+const secretCode = "burgues";
+
+function keyUp(e) {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join("").includes(secretCode)) {
+    console.log("DING DING!");
+    cornify_add();
+  }
+}
+
+window.addEventListener("keyup", keyUp);
